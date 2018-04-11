0e7f8d5fbd6397c0ebafde65757a619d55f41cac
Thieres Pereira <thieres@gmail.com>
2018-04-11 17:36:39 +0100

    .

--- login.php
+++ login.php
@@ -153,8 +153,8 @@
                         <img id="captcha" src="securimage/securimage_show.php" alt="CAPTCHA Image"/><br>
                         <a href="#" onclick="document.getElementById('captcha').src = 'securimage/securimage_show.php?' + Math.random(); return false">[Refresh Code]</a>
                     </center>
-                    -->
                     <input type="text" name="captcha_code" size="10" maxlength="6" />
+                    -->
                     <button type="submit" data-transition="slide" class="ui-btn ui-icon-check ui-btn-icon-left ui-btn-b">Login</button>
                     <a href="index.php" data-transition="slide" class="ui-btn ui-icon-arrow-l ui-btn-icon-left ui-btn-b" >Return</a>
                 </form>
