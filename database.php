f8bed8d7a5eb8345dff52eb5cb7c0bc927681a4c
Thieres Pereira <thieres@gmail.com>
2018-04-19 17:38:52 +0100

    Heroku DB Test

--- database.php
+++ database.php
@@ -4,13 +4,13 @@
         //Type of DB
         $DB_TYPE = 'mysql';
         //Host Name
-        $DB_HOST = 'localhost';
+        $DB_HOST = 'eu-cdbr-west-02.cleardb.net';
         //Host Username
-        $DB_USER = 'root';
+        $DB_USER = 'b0ca8bf99caa9b';
         //Host Password
-        $DB_PASS = '';
+        $DB_PASS = '58c6a701';
         //DB Name
-        $DB_NAME = 'pocketwaiter';
+        $DB_NAME = 'heroku_4d5a25fca0bc072';
 
         $connStr = "mysql:host=".$DB_HOST.";port=3306;dbname=".$DB_NAME;
         $DBH = new PDO($connStr,$DB_USER,$DB_PASS);
